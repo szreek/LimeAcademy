7a3af1896633ec19feb78070277b2fd41b61724d
szweda.luke@gmail.com <luke@192.168.1.11>
2022-10-24 18:05:22 +0200

    changing own Ownable for OpenZeppelin

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 pragma abicoder v2;
-import "./Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Library is Ownable {
 
