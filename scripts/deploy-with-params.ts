ef7955776ef6f325f92ebbe274a3d62ab63c3134
szweda.luke@gmail.com <luke@192.168.1.11>
2022-10-24 21:30:21 +0200

    adding deployment scripts and configuration to trigger them

--- scripts/deploy-with-params.ts
+++ scripts/deploy-with-params.ts
@@ -1,14 +1,21 @@
 import { ethers } from "hardhat";
 import hre from 'hardhat';
 
-async function deployLibrary() {
+
+async function deployLibrary(_privateKey: any) {
   await hre.run('compile');
   const [deployer] = await ethers.getSigners();
+
+  const wallet = new ethers.Wallet(_privateKey, hre.ethers.provider) // New wallet with the privateKey passed from CLI as param
+
+  console.log('Deploying contracts with the account:', wallet.address); // We are printing the address of the deployer
+  console.log('Account balance:', (await wallet.getBalance()).toString()); // We are printing the account balance
+
 
   console.log('Deploying contracts with the account:', deployer.address); // We are printing the address of the deployer
   console.log('Account balance:', (await deployer.getBalance()).toString()); // We are printing the account balance
 
-  const Library = await ethers.getContractFactory("Library");
+  const Library = await ethers.getContractFactory("Library", wallet);
   const library = await Library.deploy();
   await library.deployed();
   console.log("Library deployed to:", library.address);
